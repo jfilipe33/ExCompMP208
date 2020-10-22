c0c0711505c00e098984ff2eb1208a9fab4279ca
João Filipe Silva <joaofilipe@ita.br>
2020-10-22 20:53:37 -0300

    v1.1
    Correção dos loops

--- Exercício 1/Scripts/LS.m
+++ Exercício 1/Scripts/LS.m
@@ -9,11 +9,19 @@
 %                                                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function ls = LS(sys,ls,i)
+function ls = LS(sys,ls,n)
     
-    ls.A = ls.A + sys.H'*sys.W*sys.H;
-    ls.B = ls.B + sys.H'*sys.W*sys.y;
-    ls.thetaLS(:,i) = inv(ls.A)*(ls.B);
-    ls.RMS_LS = ls.RMS_LS + ((ls.thetaLS(:,i)-sys.theta)*(ls.thetaLS(:,i)-sys.theta)');
+    ls.A = zeros(2,2);              %Auxiliary LS Sum Matrix
+    ls.B = zeros(2,1);              %Auxiliary LS Sum Matrix
+    
+    for m = 1:sys.N                                     %Measurement Batch Loop
+        v = sys.R*randn(1);                             %Measurement Noise 
+        sys.y(m,1) = sys.H(m,:)*sys.theta + v;          %Measurement Equation
+        ls.A = ls.A + sys.H(m,:)'*sys.W*sys.H(m,:);
+        ls.B = ls.B + sys.H(m,:)'*sys.W*sys.y(m,1);
+    end
+    
+    ls.thetaLS(:,n) = inv(ls.A)*(ls.B);
+    ls.RMS_LS = ls.RMS_LS + ((ls.thetaLS(:,n)-sys.theta)*(ls.thetaLS(:,n)-sys.theta)');
 
 end
